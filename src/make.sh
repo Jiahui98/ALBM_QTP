fd3af5e7a43b251a052550725df3b5ba374b2235
Jiahui98 <jiahui.lin@nateko.lu.se>
2026-01-19 12:43:04 +0100

    Add files via upload

--- src/make.sh
+++ src/make.sh
@@ -5,21 +5,19 @@
 ##------------
 ##
 
-module purge
-
-export NETCDF_HOME=/people/tanz151/packages/Pnetcdf_mvapich_2.3.1
+export NETCDF_HOME=/home/jiahui/local/pnetcdf
 export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
-module load intel/19.0.3 mvapich2/2.3.1
-
-export FC=ifort
+export PATH=/usr/local/bin/:$PATH
 
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
-   gmake -j8
+   make OPT=0 -j8
+elif [ $arg = 'trace' ]; then
+   make TRACE=1 -j8
 elif [ $arg = 'clean' ]; then
-   gmake $arg -j8
+   make $arg -j8
 elif [ $arg = 'debug' ]; then
-   gmake DEBUG=1 -j8
+   make DEBUG=1 TRACE=1 -j8
 else
    echo "Wrong Argument: $1!!!"
 fi
